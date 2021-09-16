c3081b4b80ed76816258618050a933270014fda8
c-siudakac <kacper.siuda@efikodo.com>
2021-09-16 20:25:05 +0000

    Add validators

--- users/user.routes.config.ts
+++ users/user.routes.config.ts
@@ -2,6 +2,8 @@ import { CommonRoutesConfig } from "../common/common.routes.config";
 import express from 'express';
 import userController from "./controllers/user.controller";
 import usersMiddleware from "./middleware/users.middleware";
+import bodyValidationMiddleware from "../common/middleware/body.validation.middleware";
+import { body } from 'express-validator';
 
 export class UserRoutes extends CommonRoutesConfig {
 
@@ -14,7 +16,11 @@ export class UserRoutes extends CommonRoutesConfig {
             .route('/users')
             .get(userController.listUsers)
             .post(
-                usersMiddleware.validateRequiredUserBodyFields,
+                body('email').isEmail(),
+                body('password')
+                    .isLength({min: 5})
+                    .withMessage(`Must include password (5+ characters)`),
+                bodyValidationMiddleware.verifyBodyFieldsErrors,
                 usersMiddleware.checkIfEmailExistsInDatabase,
                 userController.createUser
             )
